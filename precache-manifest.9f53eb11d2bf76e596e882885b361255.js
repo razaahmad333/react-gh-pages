d43ff374cb74196ef8b64e0fe3843838c38050f9
ahmad raza <ahmadraza82504@gmail.com>
2020-08-16 01:58:39 +0530

    Updates

---
+++ precache-manifest.9f53eb11d2bf76e596e882885b361255.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5de1141e451f3298e1ddae3312abdfe",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "34abea0a007cfd376dd5",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "526a3a5114a5ea8b7f15",
+    "url": "/react-gh-pages/static/js/2.6f6d8ea3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.6f6d8ea3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34abea0a007cfd376dd5",
+    "url": "/react-gh-pages/static/js/main.668afa3f.chunk.js"
+  },
+  {
+    "revision": "6a091b64e2dcc40218c1",
+    "url": "/react-gh-pages/static/js/runtime-main.59eafbf8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
